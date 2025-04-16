15c07ee3e17ec7f7661c217f982076737f1c9062
babblex <szucs_alexandra@hotmail.com>
2025-04-16 14:03:42 +0200

    Lagt till felhanteringar
    404 page not found
    och
    500 interna server fel

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template ("index.html")
 
+@app.errorhandler(404)
+def error_page_not_found(error):
+    return "Sorry, the page you are looking for does not exist.", error
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "An internal python error occurred. Please contact the tech team!", error
+
 if __name__ == "__main__":
     app.run()
